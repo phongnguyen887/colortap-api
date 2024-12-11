f4109546db8eac30edcaf78a9022698568c1ef42
Phong Nguyen <phongnguyen887@csu.fullerton.edu>
2024-12-11 19:09:19 +0000

    Add: init.py

---
+++ backend/app/__init__.py
@@ -0,0 +1,5 @@
+# backend/app/__init__.py
+
+# This empty file is sufficient to mark the directory as a Python package
+# If needed, you can add package-level imports or configurations here
+from . import main, database, models, schemas, crud
